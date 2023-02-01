b729f9a50b142ea61d8ba80de41303e5d99d9912
Mohammed Jaseem Tp <56159302+Mohammedjaseem@users.noreply.github.com>
2023-02-01 12:16:05 +0530

    added

--- Downloder_app/urls.py
+++ Downloder_app/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('CheckRadis', views.checkradis, name="CheckRadis"),
-    path('add/', views.add_task, name='add_task'),
+    path('', views.add_task, name='add_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('', views.task_list, name='task_list'),
+    path('/list', views.task_list, name='task_list'),
 ]
